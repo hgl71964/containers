a460b9a1879ced6025da73f94b0bbd55a2a861a1
Mac station <guoliang.he@s-cube.com>
2021-03-11 12:59:24 +0000

    add installation

---
+++ py38_ML_dev/src/install.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+# this scripts install common software
+
+# the current script path
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# ---------------------------------------------------------------------------------------
+# for YCM in vim
+apt install build-essential cmake vim-nox python3-dev --yes
+apt install mono-complete golang nodejs default-jdk npm --yes
+
+cd ~/.vim/pack/autoplugs/start/ycm
+
+# ---------------------------------------------------------------------------------------
