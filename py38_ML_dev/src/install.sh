c03680e9a05fed8c32a4a1f3b780be447791f14f
Mac station <guoliang.he@s-cube.com>
2021-03-11 13:02:53 +0000

    lazy_commit

--- py38_ML_dev/src/install.sh
+++ py38_ML_dev/src/install.sh
@@ -10,6 +10,7 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 apt install build-essential cmake vim-nox python3-dev --yes
 apt install mono-complete golang nodejs default-jdk npm --yes
 
-cd ~/.vim/pack/autoplugs/start/ycm
+# TODO exec git submodule in a pth
+~/.vim/pack/autoplugs/start/ycm
 
 # ---------------------------------------------------------------------------------------
